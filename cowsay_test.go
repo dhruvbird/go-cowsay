5ca0a706d688cc7b1b7796430012fb23632ab6e5
Dhruv Matani <dhruvbird@gmail.com>
2013-10-19 15:46:45 -0700

    Reformat using 'go fmt'

--- cowsay_test.go
+++ cowsay_test.go
@@ -1,29 +1,29 @@
 package cowsay
 
 import (
-       "fmt"
-       "testing"
+	"fmt"
+	"testing"
 )
 
 func TestCowsay(t *testing.T) {
-     str := Format("Hello    DDD \n\n   World")
-     fmt.Printf("%s\n", str)
+	str := Format("Hello    DDD \n\n   World")
+	fmt.Printf("%s\n", str)
 
-     str = Format(fmt.Sprint("This is a really long string that ",
-     	 "has a lot of words and should span 2 ",
-	 "lines."))
-     fmt.Printf("%s\n", str)
+	str = Format(fmt.Sprint("This is a really long string that ",
+		"has a lot of words and should span 2 ",
+		"lines."))
+	fmt.Printf("%s\n", str)
 
-     str = Format(fmt.Sprint("This is a really long string that ",
-     	 "has a lot more words than the previous line and should ",
-	 "span more than 2 lines."))
-     fmt.Printf("%s\n", str)
+	str = Format(fmt.Sprint("This is a really long string that ",
+		"has a lot more words than the previous line and should ",
+		"span more than 2 lines."))
+	fmt.Printf("%s\n", str)
 
-     str = Format(fmt.Sprint("ThisLineContainsAReallyLongWord-",
-     	 "supercalifragilisticexpialidocious"))
-     fmt.Printf("%s\n", str)
+	str = Format(fmt.Sprint("ThisLineContainsAReallyLongWord-",
+		"supercalifragilisticexpialidocious"))
+	fmt.Printf("%s\n", str)
 
-     // Test empty string
-     str = Format(fmt.Sprint(""))
-     fmt.Printf("%s\n", str)
+	// Test empty string
+	str = Format(fmt.Sprint(""))
+	fmt.Printf("%s\n", str)
 }
